770155560cbeeca69b34a39a610679b624c6205e
Gavka Serhiy <sgavka@gmail.com>
2020-11-20 16:17:59 +0200

    let debugging;

--- src/run.py
+++ src/run.py
@@ -2,6 +2,13 @@
 """
 Shortcut script for calling app's main() function.
 """
+import os
+
 from app import main
 
+if os.environ.get('RUN_MAIN') == 'true':
+    HOST_IP = os.environ.get('EXTERNAL_IP')
+    import pydevd_pycharm
+
+    pydevd_pycharm.settrace(HOST_IP, port=8001, stdoutToServer=True, stderrToServer=True)
 main.main()
